48d6a669a1ef9e45740dc53c6c96b038cbdfb15d
toyowata <toyomasa.watarai@arm.com>
2020-01-21 23:50:58 +0900

    Add AS289R2 printer support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <string.h>
+
 #include "mbed.h"
 #include "RCS620S.h"
-#include "millis.h"
+#include "AS289R2.h"
 
 //#define COMMAND_TIMEOUT  400
 #define PUSH_TIMEOUT    2100
@@ -19,111 +21,193 @@
 #define NANACO_SERVICE_CODE           0x564F
 #define WAON_SERVICE_CODE             0x680B
 
+#define PRINT_ENTRIES                 10    // Max. 20
+
 int requestService(uint16_t serviceCode);
 int readEncryption(uint16_t serviceCode, uint8_t blockNumber, uint8_t *buf);
-void printBalanceLCD(char *card_name, uint32_t *balance);
+void printBalanceLCD(const char *card_name, uint32_t *balance);
 
 DigitalOut led(LED1);
 RCS620S rcs620s(RCS620S_TX, RCS620S_RX);
+AS289R2 tp(AS289R2_TX, AS289R2_RX);
 
 void parse_history(uint8_t *buf)
 {
-        for(int i = 0; i < 16; i++) {
-            printf("%02X ", buf[i]);
-        }
-        printf("\n");
+    char info[80], info2[20];
 
-        printf("機種種別: ");
-        switch (buf[0]) {
-            case 0x03:
-                printf("のりこし精算機\n");
+    for(int i = 0; i < 16; i++) {
+        printf("%02X ", buf[i]);
+    }
+    printf("\n");
+
+    sprintf(info, "機種種別: ");
+    switch (buf[0]) {
+        case 0x03:
+            strcat(info, "のりこし精算機\r");
+            break;
+        case 0x12:
+            strcat(info, "自動券売機\r");
+            break;
+        case 0x15:
+            strcat(info, "定期券発売機\r");
+            break;            
+        case 0x16:
+            strcat(info, "自動改札機\r");
+            break;
+        case 0x17:
+            strcat(info, "簡易改札機\r");
+            break;
+        case 0x18:
+            strcat(info, "駅務機器\r");
+            break;
+        case 0x46:
+            strcat(info, "ビューアルッテ端末\r");
+            break;
+        case 0xc7:
+        case 0xc8:
+            strcat(info, "物販端末\r");
+            break;
+        default:
+            strcat(info, "不明\r");
+            break;
+    }
+    printf("%s", info);
+    // tp.printf("%s", info);
+
+    sprintf(info, "利用種別: ");
+    switch (buf[1]) {
+        case 0x01:
+            strcat(info, "自動改札出場\r");
+            break;
+        case 0x02:
+            strcat(info, "SFチャージ\r");
+            break;
+        case 0x03:
+            strcat(info, "きっぷ購入\r");
+            break;
+        case 0x04:
+            strcat(info, "磁気券精算\r");
+            break;
+        case 0x05:
+            strcat(info, "乗越精算\r");
+            break;
+        case 0x06:
+            strcat(info, "窓口精算\r");
+            break;
+        case 0x07:
+            strcat(info, "新規\r");
+            break;
+        case 0x08:
+            strcat(info, "控除\r");
+            break;
+        case 0x0D:
+            strcat(info, "バス\r");
+            break;
+        case 0x0F:
+            strcat(info, "バス\r");
+            break;
+        case 0x14:
+            strcat(info, "オートチャージ\r");
+            break;
+        case 0x46:
+            strcat(info, "物販\r");
+            break;
+        default:
+            strcat(info, "不明\r");
+            break;
+    }
+    printf("%s", info);
+    tp.printf("%s", info);
+
+    if (buf[2] != 0) {
+        sprintf(info, "支払種別: ");
+        switch (buf[2]) {
+            case 0x02:
+                strcat(info, "VIEW\r");
                 break;
-            case 0x12:
-                printf("自動券売機\n");
+            case 0x0B:
+                strcat(info, "PiTaPa\r");
                 break;
-            case 0x16:
-                printf("自動改札機\n");
+            case 0x0d:
+                strcat(info, "オートチャージ対応PASMO\r");
                 break;
-            case 0x17:
-                printf("簡易改札機\n");
-                break;
-            case 0x18:
-                printf("駅務機器\n");
-                break;
-            case 0x46:
-                printf("ビューアルッテ端末\n");
-                break;
-            case 0xc7:
-            case 0xc8:
-                printf("物販端末\n");
+            case 0x3f:
+                strcat(info, "モバイルSuica\r");
                 break;
             default:
-                printf("不明\n");
+                strcat(info, "不明\r");
                 break;
         }
+        printf("%s", info);
+        tp.printf("%s", info);
+    }
 
-        printf("利用種別: ");
-        switch (buf[1]) {
+    if (buf[1] == 0x01 || buf[1] == 0x14) {
+        sprintf(info, "入出場種別: ");
+        switch (buf[3]) {
             case 0x01:
-                printf("自動改札出場\n");
+                strcat(info, "入場");
                 break;
             case 0x02:
-                printf("SFチャージ\n");
+                strcat(info, "出場");
                 break;
             case 0x03:
-                printf("きっぷ購入\n");
+                strcat(info, "定期入場");
                 break;
-            case 0x07:
-                printf("新規\n");
+            case 0x04:
+                strcat(info, "定期出場");
                 break;
-            case 0x14:
-                printf("オートチャージ\n");
+            case 0x0E:
+                strcat(info, "窓口出場");
                 break;
-            case 0x46:
-                printf("物販\n");
+            case 0x0F:
+                strcat(info, "バス入出場");
+                break;
+            case 0x12:
+                strcat(info, "料金定期");
+                break;
+            case 0x17:
+            case 0x1D:
+                strcat(info, "乗継割引");
+                break;
+            case 0x21:
+                strcat(info, "バス等乗継割引");
                 break;
             default:
-                printf("不明\n");
+                strcat(info, "不明");
                 break;
         }
+        printf("%s\r", info);
+        tp.printf("%s\r", info);
+    }
 
-        if (buf[2] != 0) {
-            printf("支払種別: ");
-            switch (buf[2]) {
-                case 0x02:
-                    printf("VIEW\n");
-                    break;
-                case 0x0B:
-                    printf("PiTaPa\n");
-                    break;
-                case 0x0d:
-                    printf("オートチャージ対応PASMO\n");
-                    break;
-                case 0x3f:
-                    printf("モバイルSuica(VIEW決済以外)\n");
-                    break;
-                default:
-                    printf("不明\n");
-                    break;
-            }
-        }
+    sprintf(info, "処理日付: %d/%02d/%02d", 2000+(buf[4]>>1), ((buf[4]&1)<<3 | ((buf[5]&0xe0)>>5)), buf[5]&0xf);
+    if (buf[1] == 0x46) {   // 物販
+        sprintf(info2, " %02d:%02d:%02d", (buf[6] & 0xF8) >> 3, ((buf[6] & 0x7) >> 5) | ((buf[7] & 0xe0) >> 5), (buf[7] & 0x1f));
+        strcat(info, info2);
+    }
+    strcat(info, "\r");
+    printf("%s", info);
+    tp.printf("%s", info);
 
-        printf("処理日付: %d/%02d/%02d\n", 2000+(buf[4]>>1), ((buf[4]&1)<<3 | ((buf[5]&0xe0)>>5)), buf[5]&0xf);
-        setlocale(LC_NUMERIC,"ja_JP.utf8");
-        printf("残額: %d円\n", (buf[11]<<8) + buf[10]); 
-        printf("\n");
+    sprintf(info, "残額: %d円\r\r", (buf[11]<<8) + buf[10]); 
+    printf("%s\n", info);
+    tp.printf("%s", info);
+    
 }
-
-#if 1
 
 int main()
 {
+    uint8_t buffer[20][16];
     printf("\n*** RCS620S テストプログラム ***\n\n");
     rcs620s.initDevice();
+    tp.initialize();
+    tp.putLineFeed(2);
 
     while(1) {
         uint32_t balance;
         uint8_t buf[RCS620S_MAX_CARD_RESPONSE_LEN];
+        int isCaptured = 0;
         
         rcs620s.timeout = COMMAND_TIMEOUT;
         
@@ -131,8 +215,9 @@ int main()
         if(rcs620s.polling(CYBERNE_SYSTEM_CODE)){
             // Suica PASMO
             if(requestService(PASSNET_SERVICE_CODE)){
+                isCaptured = 1;
                 for (int i = 0; i < 20; i++) {
-                    if(readEncryption(PASSNET_SERVICE_CODE, i, buf) && buf[12] != 0){
+                    if(readEncryption(PASSNET_SERVICE_CODE, i, buf) /*&& buf[12] != 0*/){
 #if 0
                         // Little Endianで入っているPASSNETの残高を取り出す
                         balance = buf[23];                  // 11 byte目
@@ -140,7 +225,10 @@ int main()
                         // 残高表示
                         //printBalanceLCD("PASSNET", &balance);
 #else
-                        parse_history(&buf[12]);
+                        memcpy(buffer[i], &buf[12], 16);
+                        //parse_history(&buf[12]);
+                        //parse_history(&buffer[i][0]);
+
 #endif
                     }
                 }
@@ -199,44 +287,21 @@ int main()
 */
         }
         
+        if (isCaptured) {
+            for (int i = 0; i < PRINT_ENTRIES; i++) {
+                if (buffer[i][0] != 0) {
+                    parse_history(&buffer[i][0]);
+                }
+            }
+            tp.putLineFeed(4);
+            isCaptured = 0;
+        }
+
         rcs620s.rfOff();
         led = !led;
         thread_sleep_for(POLLING_INTERVAL);
     }
 }
-#else
-int main() {
-    AnalogIn ain(dp2);
-
-    led = 1;
-    rcs620s.initDevice();
-    rcs620s.timeout = COMMAND_TIMEOUT;
-
-
-    int pool = 1;
-    while(1) {
-        float f = ain.read();
-        //printf("val = %5.2f\n", f);
-        rcs620s.timeout = COMMAND_TIMEOUT;
-        
-        // サイバネ領域
-        if(rcs620s.polling(CYBERNE_SYSTEM_CODE)){
-
-        }
-        pool = (int)(f * 100 + 1);
-        thread_sleep_for(pool);
-        rcs620s.rfOff();
-        thread_sleep_for(pool);
-        /*
-        pool += 10;
-        if (pool > 1000) {
-            pool = 1;
-        }
-        */
-    }
-}
-
-#endif
 
 // request service
 int requestService(uint16_t serviceCode){
@@ -283,6 +348,7 @@ int readEncryption(uint16_t serviceCode, uint8_t blockNumber, uint8_t *buf){
     return 1;
 }
 
-void printBalanceLCD(char *card_name, uint32_t *balance){
-    printf("%s: %d\n", card_name, *balance);
+void printBalanceLCD(const char *card_name, uint32_t *balance)
+{
+    printf("%s: %ld\n", card_name, *balance);
 }
